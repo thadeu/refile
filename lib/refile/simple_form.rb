dacbd8fd6dcac3901b784df578c95e8548ab2eb1
thadeu <tadeuu@gmail.com>
2025-01-28 19:39:28 +0000

    added object to simple_form wrapper

--- lib/refile/simple_form.rb
+++ lib/refile/simple_form.rb
@@ -1,10 +1,10 @@
 # make attachment_field behave like a normal input type so we get nice wrapper and labels
-# <%= f.input :cover_image, as: :attachment, direct: true, presigned: true %>
+# <%= f.input :cover_image, as: :attachment, direct: true, presigned: true, object: f.object %>
 module SimpleForm
   module Inputs
     class AttachmentInput < Base
       def input(wrapper_options = nil)
-        refile_options = [:presigned, :direct, :multiple]
+        refile_options = [:presigned, :direct, :multiple, :object]
         merged_input_options = merge_wrapper_options(input_options.slice(*refile_options).merge(input_html_options), wrapper_options)
         @builder.attachment_field(attribute_name, merged_input_options)
       end
